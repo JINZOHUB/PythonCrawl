6df72992e91bb46405a72aff18ba21a3ca53d5c5
J.H Song <songstudyzone@gmail.com>
2021-01-22 22:35:29 +0900

    section02 fixed code to correct

--- section02-3.py
+++ section02-3.py
@@ -22,9 +22,14 @@ def scrape_news_list_page(response):
     urls=[]
 
     root = lxml.html.fromstring(response.content)
-
-
-    for a in root.cssselect('.thumb_area .thumb_box _NM_NEWSSTAND_THUMB _NM_NEWSSTAND_THUMB_press_valid .popup_wrap a.btn_popup'):
+   # 되게 코드를 수정한 것들
+   #  // *[ @ id = "NM_NEWSSTAND_DEFAULT_THUMB"] / div[1] / div[1] / div / div[2] / div[1] / div / a[3]
+   #  # NM_NEWSSTAND_DEFAULT_THUMB > div._NM_UI_PAGE_CONTAINER > div:nth-child(1) > div > div.thumb_area > div:nth-child(1) > div > a:nth-child(3)
+   #
+   # ".thumb_area .thumb_box._NM_NEWSSTAND_THUMB._NM_NEWSSTAND_THUMB_press_valid .popup_wrap a.btn_popup:last-child"
+   #  a[data-clk="logo"]
+   #  '.thumb_area .thumb_box _NM_NEWSSTAND_THUMB _NM_NEWSSTAND_THUMB_press_valid .popup_wrap a.btn_popup' 안되는 내가 짠 코드
+    for a in root.cssselect('a[data-clk="logo"] '):
         url = a.get('href')
         urls.append(url)
 
